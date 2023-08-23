9db372779279ec6a64ee97f41a6594d54daf4fd8
Abhiraman-S-Nair <84242314+Abhiraman-S-Nair@users.noreply.github.com>
2023-08-23 23:20:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const colorButton = document.getElementById("colorButton");
-const body = document.body;
-
-colorButton.addEventListener("click", function () {
-    body.style.backgroundColor = "#001F3F"; // Deep blue night sky color
-});
