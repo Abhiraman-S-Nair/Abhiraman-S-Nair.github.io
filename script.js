02ca6ce4768c96421679c76f2d789a83b1fff7dd
Abhiraman-S-Nair <84242314+Abhiraman-S-Nair@users.noreply.github.com>
2023-08-23 22:26:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const colorButton = document.getElementById("colorButton");
+const body = document.body;
+
+colorButton.addEventListener("click", function () {
+    body.style.backgroundColor = "#001F3F"; // Deep blue night sky color
+});
