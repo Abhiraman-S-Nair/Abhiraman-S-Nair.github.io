368a45d0f8a1f8f81965e4785d5e87d57987af27
Abhiraman-S-Nair <84242314+Abhiraman-S-Nair@users.noreply.github.com>
2024-05-12 09:08:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const colorButton = document.getElementById("colorButton");
-const body = document.body;
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
 
-colorButton.addEventListener("click", function () {
-    body.style.backgroundColor = "#001F3F"; // Deep blue night sky color
+hamburger.addEventListener('click', function () { 
+	const hamIcon = this.querySelector('.hamburger-icon'); 
+	const crossIcon = this.querySelector('.cross-icon'); 
+	if (hamIcon.style.display === "none") { 
+		hamIcon.style.display = "inline-block"
+		menu.style.display = "none"
+		crossIcon.style.display = "none"
+	} 
+	else { 
+		crossIcon.style.display = "inline-block"
+		hamIcon.style.display = "none"
+		menu.style.display = "block"
+	} 
 });
